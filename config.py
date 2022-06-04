f9fcf733704e1c1c064b3d73c73a36cf0472d9b8
subashvfc <88789785+subashvfc@users.noreply.github.com>
2022-06-04 09:33:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,14 +8,14 @@ import os
 class Config(object):
 
     # Get a bot token from botfather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("5404778856:AAG4IObcdt7PIR7MQLuRZ74vnb9vGT4vFjk", "")
 
 
     # Get from my.telegram.org (or @UseTGXBot)
-    APP_ID = int(os.environ.get("APP_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
+    APP_ID = int(os.environ.get("17545386", ""))
+    API_HASH = os.environ.get("f1d5ee6adccd34db0aab30115f4c30db", "")
 
 
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("989262779", "").split())
     
